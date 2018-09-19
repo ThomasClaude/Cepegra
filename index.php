9c435b8472ac41784a2bbd4bcd01aa191c9cb502
stagiaire <stagiaire@cpg-429392.cepegra2008.lan>
2018-09-19 09:43:26 +0200

    update

--- index.php
+++ index.php
@@ -1,3 +1,85 @@
+
+<!-- Connection php  -->
+
+<?php
+$dsn = 'mysql:host=localhost;dbname=cepegra';
+$dbUser = "root";
+$dbPassword = "root";
+try {
+    $connection = new PDO($dsn, $dbUser, $dbPassword);
+    $connection->exec('SET NAMES utf8');
+} catch (PDOException $e) {
+    print "Erreur: " . $e->getMessage();
+    exit();
+}
+  
+
+if (isset($_POST['insert_newperson'])):
+  $addStatement = $connection->prepare("INSERT INTO stagiaires SET nom = :nom");
+  $addStatement->execute(
+    array(
+      'nom' => $_POST['nom']
+    )
+  );
+  
+  $lastId = $connection->lastInsertId();
+  $addStatement = $connection->prepare("INSERT INTO c_b_o SET id_stagiaire = :id_stagiaire, id_atelier = :id_atelier");
+  $addStatement->execute(
+    array(
+      'id_atelier' => $_POST['id_atelier'],
+      'id_stagiaire' => $lastId
+    )
+  );
+  header('location:index.php'); exit;
+endif;
+if (isset($_GET['id_stagiaires'])):
+  $addStatement = $connection->prepare("DELETE FROM stagiaires WHERE id_stagiaires = :id_stagiaires");
+  $addStatement->execute(
+    array(
+      'id_stagiaires' => $_GET['id_stagiaires']
+    )
+  );
+  
+  $addStatement = $connection->prepare("DELETE FROM c_b_o WHERE id_stagiaire = :id_stagiaire");
+  $addStatement->execute(
+    array(
+      'id_stagiaire' => $_GET['id_stagiaires'],
+    )
+  );
+  header('location:index.php'); exit;
+endif;
+
+
+/* Get from table   */
+
+    // $preparedStatement = $connection->prepare('SELECT * FROM stagiaires_ateliers ');
+    // $preparedStatement->execute(
+
+    //   array(
+    //     'form' => $_GET['id_stagiaires'].'%'
+    //   )
+    // );
+    // $addStatement = $connection->prepare('INSERT INTO people (firstname, age) VALUES (:firstname, :age)');
+    // $addStatement ->execute(
+    //     array(
+    //     'firstname' => $_POST['firstname'],
+    //     'age' => $_POST['age']
+    //   )
+
+    // );
+
+    $preparedStatement = $connection->prepare('SELECT id_ateliers, ateliers.name ateliers_nom FROM ateliers');
+    $preparedStatement->execute ();
+    $results = $preparedStatement->fetchAll();
+
+    $allStatement = $connection->prepare('SELECT * FROM stagiaires_ateliers');
+    $allStatement->execute ();
+    $stagiaires = $allStatement->fetchAll();
+  ?>
+
+
+  
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -9,21 +91,7 @@
   <script src="main.js"></script>
 </head>
 
-<!-- Connection php  -->
 
-<?php
-$dsn = 'mysql:host=localhost;dbname=demo';
-$dbUser = "root";
-$dbPassword = "root";
-try {
-    $connection = new PDO($dsn, $dbUser, $dbPassword);
-    $connection->exec('SET NAMES utf8');
-} catch (PDOException $e) {
-    print "Erreur: " . $e->getMessage();
-    exit();
-}
-
-?>
 
 <body>
   <div class="flexbox">
@@ -35,128 +103,84 @@ try {
         >
           Name
           <input type="text"
-                 id="firstname"
-                 name="firstname"
+                 id="nom"
+                 name="nom"
                  class="add-people__input"
           >
         </label>
-        <label for="age"
-               class="add-people__label"
+        <select name="id_atelier"
+                id="id_ateliers"
         >
-          Age
-          <input type="number"
-                 id="age"
-                 name="age"
-                 class="add-people__input"
-          >
-        </label>
+          <?php foreach ($results as $result): ?>
+          <option value="<?php echo $result["id_ateliers"] ?>">
+            <?php echo $result["ateliers_nom"] ?>
+          </option>
+          <?php endforeach ?>
+        </select>
         <input type="submit"
                value="add"
                name="add"
                class="add-people__submit"
           >
+        <input type="hidden" name="insert_newperson">
       </form>
     </div>
-    <div class="get-people">
-      <h2>Find someone</h2>
-      <form method="get">
-        <label for="firstname"
-               class="get-people__label"
-        >
-          Name
-          <input type="text"
-                 id="firstname"
-                 name="firstname"
-                 class="get-people__input"
-          >
-        </label>
-        <input type="submit"
-               value="get"
-               name="get"
-               class="get-people__submit"
-          >
-      </form>
-    </div>
-    <div class="get-people">
-      <h2>Remove someone</h2>
-      <form method="post">
-        <label for="firstname"
-               class="get-people__label"
-        >
-          Name
-          <input type="text"
-                 id="firstname"
-                 name="firstname"
-                 class="get-people__input"
-          >
-        </label>
-        <input type="submit"
-               value="remove"
-               name="get"
-               class="get-people__submit"
-          >
-      </form>
     </div>
 
   </div>
 
-  <!-- Get, Post & Delete from table    -->
-
-  <?php
-    $preparedStatement = $connection->prepare('SELECT id, firstname, age FROM people WHERE firstname LIKE :form ');
-    $preparedStatement->execute(
-
-      array(
-        'form' => $_GET['firstname'].'%'
-      )
-    );
-    $addStatement = $connection->prepare('INSERT INTO people (firstname, age) VALUES (:firstname, :age)');
-    $addStatement ->execute(
-        array(
-        'firstname' => $_POST['firstname'],
-        'age' => $_POST['age']
-      )
-
-    );
-    $removeStatement = $connection->prepare('DELETE FROM people (firstname, age) VALUES (:firstname, :age)');
-    $removeStatement ->execute(
-        array(
-        'firstname' => $_POST['firstname'],
-        'age' => $_POST['age']
-      )
-
-    );
-    $results = $preparedStatement->fetchAll();
-  ?>
+  
 
   <table>
     <tr>
       <th>
-        ID
+        Id-Stagiaire
       </th>
       <th>
-        Name
+        Stagiaire
       </th>
       <th>
-        Age
+        Id-Atelier
+      </th>
+      <th>
+        Atelier
       </th>
     </tr>
 
-    <?php foreach ($results as $result): ?>
+    <?php foreach ($stagiaires as $stagiaire): ?>
       <tr>
         <td>
-          <?php echo $result ["id"] ?>
+          <?php echo $stagiaire["id_stagiaires"] ?>
         </td>
         <td>
-          <?php echo $result ["firstname"] ?>
+          <?php echo $stagiaire["stagiaires_nom"] ?>
         </td>
         <td>
-          <?php echo $result ["age"] ?>
+          <?php echo $stagiaire["id_ateliers"] ?>
+        </td>
+        <td>
+          <?php echo $stagiaire["ateliers_nom"] ?>
+        </td>
+        <td>
+        <a href="index.php?id_stagiaires=<?php echo $stagiaire['id_stagiaires'] ?>" data-name="<?php echo $stagiaire["stagiaires_nom"] ?>"
+      >x</a>
         </td>
       </tr>
+      
     <?php endforeach ?>
   </table>
+<script>
+  const linkElement = document.querySelectorAll('a');
+  for (let i = 0; i < linkElement.length ; i++) {
+    linkElement[i].addEventListener('click', function(e) {
+      let stagiaireName = this.getAttribute('data-name');
+      if (!confirm('Supprimer ' + stagiaireName)) {
+        e.preventDefault();
+      }
+    });
+  }
 
+</script>
 </body>
 
 
